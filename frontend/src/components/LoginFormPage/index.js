eb521dc55a59522035afa588ce50022e36e3e59b
Dez <dez@dezs-mbp.attlocal.net>
2021-02-02 18:05:12 -0500

    form social sign in add

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
-import { Redirect } from "react-router-dom";
+import { NavLink, Redirect } from "react-router-dom";
 import "./LoginForm.css";
 
 function LoginFormPage() {
@@ -25,19 +25,29 @@ function LoginFormPage() {
   };
 
   return (
-    <div className="form-container form-container__sign-in">
-      <form onSubmit={handleSubmit}>
+    <div className="form-container__sign-in">
+      <form className="form__sign-in" onSubmit={handleSubmit}>
         <h1>Sign In</h1>
         <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
+        <div className="social-signin">
+          <a href="#" class="social">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="#" class="social">
+            <i class="fab fa-google-plus-g"></i>
+          </a>
+        </div>
+        <span>or use your account</span>
         <label>
           <input
             type="text"
             value={credential}
-            placeholder="Username or Email"
+            className="username-input"
+            placeholder="Username"
             onChange={(e) => setCredential(e.target.value)}
             required
           />
@@ -46,6 +56,7 @@ function LoginFormPage() {
           <input
             type="password"
             value={password}
+            className="password-input"
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             required

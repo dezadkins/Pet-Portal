9dc49274b4dd1efff4aba15b22539a5651c71244
Dez <dez@dezs-mbp.attlocal.net>
2021-02-02 22:11:05 -0500

    form fix

--- frontend/src/components/LoginPage/index.js
+++ frontend/src/components/LoginPage/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import LoginFormPage from "../LoginFormPage";
 import SignupFormPage from "../SignupFormPage";
 
 import "./LoginPage.css";
 
+// return (
+//   <CSSTransition
+//     in={switch}
+//     classNames="switch"
+//     onClick={() => setSwitch(!switch)}
+//     timeout={0}
+//   ></CSSTransition>
+
 export default function LoginPage() {
+  const [switch, setSwitch] = useState(True);
+  const [signing, setSigning] = useState(True);
+  useEffect(() => {
+    toggle
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark");
+  }, [toggle]);
+
   return (
     <div className="page-container">
+      <h1 className="login-title">Pet Portal</h1>
       <div className="container">
         <div className="form-container sign-up-container">
           <SignupFormPage />
@@ -21,14 +38,14 @@ export default function LoginPage() {
               <p>
                 To keep connected with us please login with your personal info
               </p>
-              <button class="shadow" id="signIn">
+              <button class="ghost" id="signIn">
                 Sign In
               </button>
             </div>
             <div class="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button class="ghost" id="signUp">
+              <h1>Greetings</h1>
+              <p>Sign up to start your Pet Portal</p>
+              <button class="buddy ghost" id="signUp">
                 Sign Up
               </button>
             </div>

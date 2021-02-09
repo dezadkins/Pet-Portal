82d1af1785b1671d2a48403a451b9e8f150b02ff
Dez <dez@dezs-mbp.attlocal.net>
2021-02-09 14:52:30 -0500

    Chart comp add

---
+++ frontend/src/components/Chart/Chart.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { Doughnut } from "react-chartjs-2";
+import axios from "axios";
+
+const Chart = () => {
+  return (
+    <>
+      <h1> Chart</h1>
+    </>
+  );
+};
+
+export default Chart;

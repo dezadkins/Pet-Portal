251941336917d71dcf453debce73bdf26cd7f21f
Dez <dez@dezs-mbp.attlocal.net>
2021-02-09 14:23:29 -0500

    calendar functionality add

--- frontend/src/components/Calendar/Calendar.js
+++ frontend/src/components/Calendar/Calendar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import FullCalendar from "@fullcalendar/react";
+import FullCalendar, { applyStyle } from "@fullcalendar/react";
 import dayGrid from "@fullcalendar/daygrid";
 import timeGrid from "@fullcalendar/timegrid";
 import interaction from "@fullcalendar/interaction";
@@ -8,7 +8,7 @@ import axios from "axios";
 const Calendar = () => {
   const [sessionArray, setSessionArray] = useState([]);
   const [sessions, setSessions] = useState([]);
-  const [appt, setAppts] = useState([]);
+  const [appts, setAppts] = useState([]);
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -25,9 +25,76 @@ const Calendar = () => {
       .catch((error) => console.log(error.toString()));
   }, [isLoaded]);
 
+  useEffect(() => {
+    const initArray = [];
+    appts.forEach((appt) => {
+      let date = Date.parse(appt.dueDate);
+      let datePlus1 = date + 172800000;
+      let formattedDate = new Date(datePlus1).toISOString();
+      initArray.push({
+        title: `DUE: ${appt.name}`,
+        id: appt._id,
+        start: formattedDate,
+        allDay: true,
+        color: "purple",
+      });
+    });
+    sessions.forEach((session) => {
+      if (session.sessionType === "planned") {
+        initArray.push({
+          title: `Planned:${session.description}-${session.apptName}`,
+          id: session._id,
+          start: session.start[0],
+          end: session.end[0],
+          allDay: session.allDay,
+          color: "orange",
+        });
+      } else {
+        initArray.push({
+          title: `${session.description}-${session.apptName}`,
+          id: session._id,
+          start: session.start[0],
+          end: session.end[0],
+          allDay: session.allDay,
+          color: "blue",
+        });
+      }
+    });
+    setSessionArray(initArray);
+  }, [sessions, appts]);
+
+  const handleEventClick = (clickInfo) => {
+    if (
+      window.confirm(
+        `Are you sure you want to delete the appointment '${clickInfo.appt.title}'`
+      )
+    ) {
+      clickInfo.appt.remove();
+      axios
+        .delete(`/api/session/${clickInfo.event._def.publicId}`)
+        .catch((error) => console.oog(error));
+    }
+  };
+
   return (
     <>
-      <div>Hello from Calendar</div>
+      <div>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          events={sessionArray}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true}
+          dayMaxEvents={true}
+          eventClick={handleEventClick}
+        />
+      </div>
     </>
   );
 };

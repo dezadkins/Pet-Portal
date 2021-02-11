67e0368de36f6c40fbf8a57f3beed8f42a1b9490
Dez <dez@dezs-mbp.attlocal.net>
2021-02-11 13:02:41 -0500

    add pet modal initial

--- frontend/src/components/PetFormModal/AddPetForm.js
+++ frontend/src/components/PetFormModal/AddPetForm.js
@@ -6,6 +6,7 @@ import "./AddPetForm.css";
 function AddPetForm() {
   const dispatch = useDispatch();
   const [credential, setCredential] = useState("");
+  const [petType, setPetType] = useState([]);
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
 
@@ -19,36 +20,63 @@ function AddPetForm() {
     );
   };
 
+  const petSubmit = (e) => {
+    e.preventDefault();
+    setPetType([]);
+    return;
+  };
+
   return (
-    <>
-      <h1>Log In</h1>
+    <div className="addPet__form">
+      <h1 className="form-title">New Addition</h1>
       <form onSubmit={handleSubmit}>
         <ul>
           {errors.map((error, idx) => (
             <li key={idx}>{error}</li>
           ))}
         </ul>
+
+        <input
+          type="text"
+          placeholder="Name"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+        />
+
         <label>
-          Username or Email
-          <input
-            type="text"
+          Pet Type:
+          <select
             value={credential}
             onChange={(e) => setCredential(e.target.value)}
-            required
-          />
+          >
+            <option value="Dog">Dog</option>
+            <option value="Cat">Cat</option>
+            <option value="Ferret">Ferret</option>
+            <option value="Horse">Horse</option>
+            <option value="Pig">Pig</option>
+          </select>
         </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
+
+        <input
+          type="text"
+          placeholder="Breed"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="D.O.B"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Submit</button>
       </form>
-    </>
+    </div>
   );
 }
 
